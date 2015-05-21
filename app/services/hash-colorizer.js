903078243c3a0c43800a6e32ef08f6e4732bf795
Yun-yu Shen <yun@guanmado.com>
2015-05-21 14:13:26 -0400

    updated color scheme to be more distinct.

--- app/services/hash-colorizer.js
+++ app/services/hash-colorizer.js
@@ -3,20 +3,20 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   colorScheme: function(id) {
     var colors0 = ['orange','deeppink','darkred','indigo'];
-    var colors1 = ['indigo','blue','teal','green'];
+    var colors1 = ['indigo','DodgerBlue','LightSeaGreen'];
 
-    var bezierAdjCol0 = chroma.interpolate.bezier(colors0),
-        bezierAdjCol1 = chroma.interpolate.bezier(colors1);
+    //colors0 = chroma.interpolate.bezier(colors0);
+    //colors0 = chroma.interpolate.bezier(colors1);
 
-    var cs0 = chroma.scale(bezierAdjCol0).mode('lab').correctLightness(true),
-        cs1 = chroma.scale(bezierAdjCol1).mode('lab').correctLightness(true);
+    var cs0 = chroma.scale(colors0).mode('lab'),
+        cs1 = chroma.scale(colors1).mode('lab');
 
     var cs = function(t) {
       if (t<0.5) return cs0(t*2);
       return cs1(t*2-1);
     };
 
-    var i=4;
+    var i=8;
 
     return cs(parseInt(md5(id).substring(i,i+13),16)/4503599627370495).hex();
   }
