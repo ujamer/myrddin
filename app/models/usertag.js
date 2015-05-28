711bcfe2eaca1ac5ea0ee953a5e40a4a925975b9
ujamer <yun@guanmado.com>
2015-05-28 00:07:51 -0400

    move the tag-user relationship to usertag

--- app/models/usertag.js
+++ app/models/usertag.js
@@ -1,9 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  docs: DS.belongsTo('document'),
-  start: DS.attr('number'),
+  docs: DS.hasMany('document'),
   tag: DS.belongsTo('tag'),
   user: DS.belongsTo('user'),
+  start: DS.attr('string'),
   isDefault: DS.attr('boolean')
 });
